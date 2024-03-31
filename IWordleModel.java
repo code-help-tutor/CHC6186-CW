40ca53494553ba029fe4af0547f31fd7c2fe3202
Code Help Tutor <1823890830@qq.com>
2024-03-31 03:17:01 +0000

    WeChat cstutorcs CHC6186 CW

---
+++ IWordleModel.java
@@ -0,0 +1,18 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import java.util.List;
+
+public interface IWordleModel {
+    int MAX_ATTEMPTS = 6;
+    List<String> VALID_WORDS = List.of("APPLE", "HAPPY", "CHERRY", "GRAPE", "SUNNY", "LEMON", "DAISY");
+
+    void initialize();
+    boolean processInput(String input);
+    boolean isGameOver();
+    boolean isGameWon();
+    String getTargetWord();
+    StringBuilder getCurrentGuess();
+    int getRemainingAttempts();
+    void startNewGame();
+}
